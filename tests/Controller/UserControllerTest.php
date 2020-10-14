d0c1a7018db388d4fdd55995608beedc1a0aa761
Geoffray <geoffray.veyssie@gmail.com>
2020-10-14 17:33:28 +0200

    Add: tets for user controller

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testRegister():void
+    {
+        $client = self::createClient();
+
+        $client->request(Request::METHOD_GET, 'en/user/registration');
+
+        self::assertResponseIsSuccessful();
+
+        $client->enableProfiler();
+
+        $client->submitForm('registration[submit]', [
+            'registration[email]'               => 'test@bikingapp.com',
+            'registration[name]'                => 'usertest',
+            'registration[password][first]'     => 'passwordtest',
+            'registration[password][second]'    => 'passwordtest',
+        ]);
+
+        if ($profile = $client->getProfile()) {
+            self::assertLessThan(
+                600,
+                $profile->getCollector('time')->getDuration()
+            );
+        }
+
+        $client->followRedirect();
+        self::assertSelectorExists('.alert-info');
+    }
+
+    public function testRegister_EmailAlreadyExists(): void
+    {
+        $client = self::createClient();
+
+        $client->request(Request::METHOD_GET, '/en/user/registration');
+
+        self::assertResponseIsSuccessful();
+
+        $client->submitForm('registration[submit]', [
+            'registration[email]'               => 'junior@mybikingapp.fr',
+            'registration[name]'                => 'usertest',
+            'registration[password][first]'     => 'passwordtest',
+            'registration[password][second]'    => 'passwordtest',
+        ]);
+
+        self::assertRouteSame('user_registration');
+        self::assertSelectorExists('.form-error-message');
+    }
+
+    public function testRegister_NameAlreadyExists(): void
+    {
+        $client = self::createClient();
+
+        $client->request(Request::METHOD_GET, '/en/user/registration');
+
+        self::assertResponseIsSuccessful();
+
+        $client->submitForm('registration[submit]', [
+            'registration[email]'               => 'testunique@mybikingapp.fr',
+            'registration[name]'                => 'User junior',
+            'registration[password][first]'     => 'passwordtest',
+            'registration[password][second]'    => 'passwordtest',
+        ]);
+
+        self::assertRouteSame('user_registration');
+        self::assertSelectorExists('.form-error-message');
+    }
+
+    public function testResiter_afterLogin(): void
+    {
+        $client = self::createClient();
+
+        $userRepository = self::$container->get(UserRepository::class);
+
+        $logged_in_user = $userRepository->findOneByEmail('junior@mybikingapp.fr');
+
+        $client->loginUser($logged_in_user);
+
+        $client->request(Request::METHOD_GET, '/en/user/registration');
+
+        self::assertEquals(302, $client->getResponse()->getStatusCode());
+
+        $client->followRedirect();
+
+        self::assertRouteSame('home');
+    }
+}
