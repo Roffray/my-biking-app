170899396f8b90467b095022b00556deacb21264
Geoffray <geoffray.veyssie@gmail.com>
2020-12-20 22:49:27 +0100

    Upates

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -5,19 +5,18 @@
 // import 'raw-loader!@gegeweb/leaflet-routing-machine-openroute'
 
 // CSS
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
-import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
-import '@raruto/leaflet-elevation/dist/leaflet-elevation.css';
-import 'leaflet-sidebar-v2/css/leaflet-sidebar.css';
+// import 'leaflet/dist/leaflet.css';
+// import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
+// import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
+// import '@raruto/leaflet-elevation/dist/leaflet-elevation.css';
+// import 'leaflet-sidebar-v2/css/leaflet-sidebar.css';
 // JS
 // import 'leaflet/dist/leaflet.js';
 // import 'leaflet-routing-machine/dist/leaflet-routing-machine.js';
-import '@gegeweb/leaflet-routing-machine-openroute/dist/leaflet-routing-openroute.js';
-import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
-import '@raruto/leaflet-elevation/dist/leaflet-elevation.min.js';
-import 'leaflet-sidebar-v2/js/leaflet-sidebar.js';
-// import 'exports-loader?type=commonjs&exports[]=multiple|L.control.sidebar|sidebar!leaflet-sidebar-v2/js/leaflet-sidebar.js';
+// import '@gegeweb/leaflet-routing-machine-openroute/dist/leaflet-routing-openroute.js';
+// import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
+// import '@raruto/leaflet-elevation/dist/leaflet-elevation.min.js';
+// import 'leaflet-sidebar-v2/js/leaflet-sidebar.js';
 
 // import 'leaflet-routing-machine/../examples/Control.Geocoder.js';
 // import 'openrouteservice-js';
